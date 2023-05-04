c2fda6aaea1923b2fcd07a7f9217e6981bb0c143
denis88k <denis88.k@mail.ru>
2023-05-04 22:33:58 +0300

    add: modal promo actual

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,10 @@
 import modal from './components/modal.js';
 
-modal('.promo__header-modal-payment', '.modal-payment', '.close-icon');
-modal('.promo__header-modal-tradIn', '.modal-tradIn', '.close-icon');
+// promo-header
+modal('.promo__header-modal-payment', '.modal-payment', '.modal-payment .modal__close');
+modal('.promo__header-modal-tradIn', '.modal-tradIn', '.modal-tradIn .modal__close');
+
+// promo-actual
+modal('.promo__header-modal-discount', '.modal-discount', '.modal-discount .modal__close');
+modal('.promo__header-modal-tiresСasco', '.modal-tiresСasco', '.modal-tiresСasco .modal__close');
+modal('.promo__header-modal-bestPrice', '.modal-bestPrice', '.modal-bestPrice .modal__close');
