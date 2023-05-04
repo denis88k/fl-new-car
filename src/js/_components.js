518cf859703863f27a9c9d24f230b40f74c4f7a0
Denis(denis88k) <denis88.k@mail.ru>
2023-05-04 14:00:10 +0300

    feat: modal in promo title

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,2 +1,4 @@
-import './components/scripts.js';
-console.log('components');
+import { modal } from './components/modal.js';
+
+modal('.promo__header-modal-payment', '.modal-payment', '.close-icon');
+modal('.promo__header-modal-tradIn', '.modal-tradIn', '.close-icon');
---
+++ src/js/components/modal.js
@@ -0,0 +1,27 @@
+export const modal = (btnOpen, modal, btnClose) => {
+  (btnOpen = document.querySelector(btnOpen)),
+    (modal = document.querySelector(modal)),
+    (btnClose = document.querySelector(btnClose));
+  const body = document.body;
+
+  btnOpen.addEventListener('click', e => {
+    e.preventDefault();
+    body.classList.add('_lock');
+    modal.classList.add('active');
+  });
+
+  btnClose.addEventListener('click', () => {
+    body.classList.remove('_lock');
+    modal.classList.remove('active');
+  });
+
+  modal.addEventListener('click', e => {
+    if (e.target === modal) {
+      body.classList.remove('_lock');
+      modal.classList.remove('active');
+    }
+  });
+};
+// btnOpen --- класс кнопки, при клике на которую будет ОТКРЫВАТЬСЯ модальное окно
+// modal --- класс открываемого модального окна
+// btnClose --- класс кнопки, при клике на которую будет ЗАКРЫВАТЬСЯ модальное окно
