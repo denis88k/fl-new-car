63e3abfb1dd8c4c67771f1a989cb5536b9040a4b
Denis(denis88k) <denis88.k@mail.ru>
2023-05-05 15:48:36 +0300

    feat: cars chat 80%

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -10,3 +10,29 @@ window.addEventListener('scroll', () => {
     consultSticky.classList.contains('show') && consultSticky.classList.remove('show');
   }
 });
+
+// прокрутка до определённого сообщения
+// https://learn.javascript.ru/size-and-scroll-window
+// false - elem внизу
+// true - elem вверху
+// elem.scrollIntoView(false)
+
+// чат
+
+const question = document.querySelector('.chat-messages');
+const msgBlocks = question.querySelectorAll('.chat__message-block');
+
+msgBlocks.forEach((msgBlock, index) => {
+  const msgPrint = msgBlock.querySelector('.chat__message-print');
+  const msgConsult = msgBlock.querySelector('.chat__message-consultant');
+  setTimeout(() => {
+    setTimeout(() => {
+      !msgPrint.classList.contains('msg-print-show') && msgPrint.classList.add('msg-print-show');
+    }, 725);
+
+    setTimeout(() => {
+      msgPrint.classList.remove('msg-print-show');
+      msgConsult.classList.add('msg-show');
+    }, 2500);
+  }, index * 2500);
+});
